86f8363f10767b432535b2a3ca782d260e587f95
AriasLcr <ariaslcr@gmail.com>
2025-12-25 20:49:36 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-full h-full place-items-center'>
+      <h1 className='font-sketch-written'>Gabriel Arias</h1>
+      <div className='h-250 grid place-items-center'>
+        hi
+      </div>
+    </div>
+  )
+}
+
+export default App
